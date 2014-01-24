83619f3302bece4be7c70796dae4c65ff8394abc
absarora <absingharora@gmail.com>
2014-01-24 15:15:05 -0800

    Implemented Cancan and basic roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124195122) do
+ActiveRecord::Schema.define(:version => 20140124221229) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20140124195122) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
