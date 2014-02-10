9e0d5de2d708f9e8437b9d6d091218e0d895b7cd
absarora <absingharora@gmail.com>
2014-02-09 21:29:40 -0800

    Implemented favoriting

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,14 @@
 require 'faker'
 
-# Create 50 topics
+# Create 15 topics
 topics = []
-50.times do
+200.times do
   topics << Topic.create(
     name: Faker::Lorem.words(rand(1..10)).join(" "), 
     description: Faker::Lorem.paragraph(rand(1..4))
   )
 end
-
-post_count = Post.count
-User.all.each do |user|
-  rand(30..50).times do
-    p = Post.find(rand(1..post_count))
-    c = user.comments.create(
-      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
-      post: p)
-    c.update_attribute(:created_at, Time.now - rand(600..31536000))
-  end
-end
+ 
 
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
@@ -30,26 +20,28 @@ rand(4..10).times do
   u.skip_confirmation!
   u.save
 
-  # Note: by calling `User.new` instead of `create`,
-  # we create an instance of a user which isn't saved to the database.
-  # The `skip_confirmation!` method sets the confirmation date
-  # to avoid sending an email. The `save` method updates the database.
-
-  rand(5..12).times do
-    topic = topics.first #getting the first topic here
+  rand(50..500).times do
+    topic = topics.first
     p = u.posts.create(
       topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
-    p.update_rank
-    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
 
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-    end
+    p.update_rank
+    topics.rotate!
+  end
+end
+
+post_count = Post.count
+User.all.each do |user|
+  rand(30..50).times do
+    p = Post.find(rand(1..post_count))
+    c = user.comments.create(
+      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+      post: p)
+    c.update_attribute(:created_at, Time.now - rand(600..31536000))
   end
 end
 
